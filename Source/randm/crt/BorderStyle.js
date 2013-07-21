ea0c2f1ebd695d7373feca452c4a50329566c1e6
Rick Parrish <github@rick.parrish>
2013-07-21 19:11:29 -0400

    Mark the ActionScript files so I know what needs to be ported still

--- Source/randm/crt/BorderStyle.js
+++ Source/randm/crt/BorderStyle.js
@@ -17,6 +17,7 @@
   You should have received a copy of the GNU General Public License
   along with HtmlTerm.  If not, see <http://www.gnu.org/licenses/>.
 */
+// TODO This is still ActionScript, not JavaScript
 package randm.crt
 {
 	/// <summary>
